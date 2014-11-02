99b73a988454e84b9bb1a3c1191ff0a8825a29cb
Hanno Schlichting <hanno@hannosch.eu>
2014-11-02 15:32:02 +0100

    prepare release

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@
 from setuptools import setup, find_packages
 
 setup(name='Products.ExternalMethod',
-      version = '2.13.1dev',
+      version = '2.13.1',
       url='http://pypi.python.org/pypi/Products.ExternalMethod',
       license='ZPL 2.1',
       description="This package provides support for external Python methods "
