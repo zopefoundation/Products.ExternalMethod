b26d76df084491b941739933a8ccf3cb4106f756
Jens Vagelpohl <jens@plyp.com>
2024-09-16 11:33:56 +0200

    - vb [ci skip]

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ from setuptools import setup
 
 
 setup(name='Products.ExternalMethod',
-      version='6.0',
+      version='6.1.dev0',
       url='https://github.com/zopefoundation/Products.ExternalMethod',
       project_urls={
           'Issue Tracker': ('https://github.com/zopefoundation/'
